6da9dc4a00db32421989ba1310d2d599536db155
Janhavi Theurkar <janhavitheurkar@gmail.com>
2025-11-26 19:27:05 +0530

    Create Str44.c

---
+++ Str44.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){ char s[100],o[100]; gets(s); int j=0;
+for(int i=0;s[i];i++) if(s[i]!='\t') o[j++]=s[i];
+o[j]='\0'; puts(o); }
+#include <stdio.h>
+int main(){ char s[100],o[100]; gets(s); int j=0;
+for(int i=0;s[i];i++) if(s[i]!='\t') o[j++]=s[i];
+o[j]='\0'; puts(o); }
